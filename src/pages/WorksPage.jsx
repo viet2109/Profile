01bfe29418535388e40bcd7dff58665e4c11bd96
Your Name <you@example.com>
2022-11-23 20:20:46 +0700

    fix bug

--- src/pages/WorksPage.jsx
+++ src/pages/WorksPage.jsx
@@ -12,37 +12,14 @@ export default function WorksPage() {
             demo: "https://duyvu-toptop.vercel.app/",
             repo: "https://github.com/Duyvu2610/toptop"
         },
-        {
-            id: 2,
-            video: "/videos/tiktok.mp4",
-            name: "-Tiktok-",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates similique eius perspiciatis quam dolor dolorem, soluta accusamus nam quidem ex magni fuga porro commodi alias esse in est repellendus iusto!",
-            demo: "https://duyvu-toptop.vercel.app/",
-            repo: "https://github.com/Duyvu2610/toptop"
-        },
-        {
-            id: 3,
-            video: "/videos/tiktok.mp4",
-            name: "-Tiktok-",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates similique eius perspiciatis quam dolor dolorem, soluta accusamus nam quidem ex magni fuga porro commodi alias esse in est repellendus iusto!",
-            demo: "https://duyvu-toptop.vercel.app/",
-            repo: "https://github.com/Duyvu2610/toptop"
-        },
-        {
-            id: 4,
-            video: "/videos/tiktok.mp4",
-            name: "-Tiktok-",
-            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates similique eius perspiciatis quam dolor dolorem, soluta accusamus nam quidem ex magni fuga porro commodi alias esse in est repellendus iusto!",
-            demo: "https://duyvu-toptop.vercel.app/",
-            repo: "https://github.com/Duyvu2610/toptop"
-        },
+
     ]
     return (
         <Content title='my works' titleBg='works'>
             <div className="grid grid-cols-2 gap-10 p-10 pb-20">
                 {
                     projects.map(e => (
-                        <div className="rounded-md flex flex-col bg-gray-800 p-5 group" key={e.id} >
+                        <div className="rounded-md flex flex-col bg-white p-5 group" key={e.id} >
                             <div className="relative overflow-hidden group">
                                 <video src={e.video} muted autoPlay loop className='px-4 ' />
                                 <div className="absolute w-full h-full bg-bg bg-opacity-50  flex justify-center items-center z-[99999] -left-full slide top-0 transition delay-500 group-hover:left-0">
@@ -55,7 +32,7 @@ export default function WorksPage() {
                                 </div>
                             </div>
                             <p className='py-4 font-bold text-2xl'>{e.name}</p>
-                            <p className='text-gray-300 '>{e.desc}</p>
+                            <p className=' '>{e.desc}</p>
                         </div>
                     ))
                 }
